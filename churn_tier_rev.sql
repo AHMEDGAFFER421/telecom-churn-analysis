390417599ffc6c11e806befc071d732c43d54169
AHMEDGAFFER421 <ahmeddesokygaffer@gmail.com>
2026-01-06 17:48:59 +0200

    Update churn_tier_rev.sql

--- churn_tier_rev.sql
+++ churn_tier_rev.sql
@@ -1,20 +1,18 @@
 SELECT
-	COUNT(*) AS churned,
-	CASE WHEN account_length_in_months < 6 THEN 'half_year'
-	WHEN account_length_in_months  BETWEEN 6 AND 12 THEN 'year'
-	WHEN account_length_in_months  BETWEEN 13 AND 24 THEN 'two_year'
-	ELSE 'more_than_2y' END AS tenure,
-	SUM(total_charges) AS tot_losses,
-	AVG(monthly_charge) AS avg_monthly_charge,
-	SUM(total_charges) / COUNT(*) AS avg_loss_per_customer,
-    COUNT(*)::float / SUM(COUNT(*)) OVER (PARTITION BY contract_type) AS churn_rate,
-	SUM(total_charges)/SUM(account_length_in_months) AS avg_monthly_per_customer,
-	
-	contract_type AS contract
-	
+    COUNT(*) AS churn_count,
+    CASE 
+        WHEN account_length_in_months < 6 THEN 'half_year'
+        WHEN account_length_in_months BETWEEN 6 AND 12 THEN 'year'
+        WHEN account_length_in_months BETWEEN 13 AND 24 THEN 'two_year'
+        ELSE 'more_than_2y'
+    END AS tenure,
+    contract_type AS contract,
+    SUM(total_charges) AS total_loss,
+    AVG(monthly_charge) AS avg_monthly_charge,
+    SUM(total_charges)/COUNT(*) AS avg_loss_per_customer,
+    COUNT(*)::float / SUM(COUNT(*)) OVER (PARTITION BY contract_type) AS churn_rate
 FROM churn
-WHERE churn_label = 'Yes'  
+WHERE churn_label = 'Yes'
 GROUP BY tenure, contract_type
-ORDER BY tot_losses DESC
-
-	
+ORDER BY total_loss DESC;
+-- Show how customer tenure and contract type relate to churn, total revenue lost, and churn rate.
