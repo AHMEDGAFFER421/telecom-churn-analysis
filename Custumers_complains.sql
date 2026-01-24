8094bc25a4d1e7aa391610fa7f9509281bdaf841
AHMEDGAFFER421 <ahmeddesokygaffer@gmail.com>
2026-01-24 14:31:59 +0200

    Revise SQL query for customer churn analysis
    Refactored SQL query to analyze customer service calls and churn rates by contract type and tenure.

--- Custumers_complains.sql
+++ Custumers_complains.sql
@@ -1,20 +1,33 @@
-SELECT
-    contract_type AS contract,
-    CASE 
-        WHEN account_length_in_months < 6 THEN 'half_year'
-        WHEN account_length_in_months BETWEEN 6 AND 12 THEN 'year'
-        WHEN account_length_in_months BETWEEN 13 AND 24 THEN 'two_year'
-        ELSE 'more_than_2y'
-    END AS tenure,
-    COUNT(*) AS total_customers,
-    COUNT(*) FILTER (WHERE churn_label = 'Yes') AS churn_count,
-    COUNT(*) FILTER (WHERE churn_label = 'Yes')::float / COUNT(*) AS churn_rate,
-    SUM(customer_service_calls) FILTER (WHERE churn_label = 'Yes') AS total_calls_churned,
-    AVG(customer_service_calls) FILTER (WHERE churn_label = 'Yes') AS avg_calls_churned,
-    SUM(customer_service_calls) FILTER (WHERE churn_label = 'No') AS total_calls_not_churned,
-    AVG(customer_service_calls) FILTER (WHERE churn_label = 'No') AS avg_calls_not_churned
-FROM churn  
-GROUP BY contract_type, tenure
-ORDER BY avg_calls_churned DESC;
---Investigate whether higher customer service calls are associated with churn, across contract types and tenure.
+WITH Cte AS (SELECT
+				customer_id,
+				churn_label,
+    			contract_type AS contract,
+				customer_service_calls,
+				account_length_in_months,
+   		 		CASE 
+        			WHEN account_length_in_months < 6 THEN 'half_year'
+        			WHEN account_length_in_months BETWEEN 6 AND 12 THEN 'year'
+        			WHEN account_length_in_months BETWEEN 13 AND 24 THEN 'two_year'
+        			ELSE 'more_than_2y'
+    			END AS tenure,
+				customer_service_calls::float / NULLIF(account_length_in_months, 0) AS avg_complaints_per_month
+				FROM churn),
+customer_complaints AS	(SELECT
+				tenure,
+				contract,
+   				COUNT(*) AS total_customers,
+    			COUNT(*) FILTER (WHERE churn_label = 'Yes') AS churn_count,
+    			COUNT(*) FILTER (WHERE churn_label = 'Yes')::float / COUNT(*) AS churn_rate,
+    			SUM(avg_complaints_per_month) FILTER (WHERE churn_label = 'Yes') AS total_complaints_per_month_churned,
+    			AVG(avg_complaints_per_month) FILTER (WHERE churn_label = 'Yes') AS avg_complaints_per_month_churned,
+    			SUM(avg_complaints_per_month) FILTER (WHERE churn_label = 'No') AS total_complaints_per_month_churned,
+    			AVG(avg_complaints_per_month) FILTER (WHERE churn_label = 'No') AS avg_complaints_per_month_non_churned
+				FROM Cte  
+				GROUP BY contract, tenure
+				ORDER BY avg_complaints_per_month_churned DESC)
+SELECT *,
+       RANK() OVER (PARTITION BY contract ORDER BY churn_rate DESC) AS rank_usage
+FROM customer_complaints
+ORDER BY contract, rank_usage;
 
+--Investigate whether higher customer service calls are associated with churn, across contract types and tenure.
